ad9fb899ae1f40945340322eac09a722c03443ba
iamkingclutch3 <diegolopezgundin@gmail.com>
2025-04-12 01:19:11 +0200

    Added custom ip 2

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const builder = require("./index");
 
 const port = process.env.PORT || 7000;
+const ip = process.env.IP || localhost;
 
 require("http")
   .createServer(builder.getInterface())
-  .listen(port, "0.0.0.0", () => {
-    console.log(`✅ Addon running on 0.0.0.0:${port}`);
+  .listen(port, ip, () => {
+    console.log(`✅ Addon running on ${ip}:${port}`);
   });
